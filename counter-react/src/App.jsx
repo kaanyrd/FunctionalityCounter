5871c2520a26ad47ff13253b60f5cd55123617d4
KAAN YARDIMCI <kaan.yardimci@yahoo.com.tr>
2022-10-23 02:53:40 +0300

    Functionality finished...

--- counter-react/src/App.jsx
+++ counter-react/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
 import Counter from './components/Counter';
 
+
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
       <Counter />
     </div>
   );
--- counter-react/src/components/Counter.jsx
+++ counter-react/src/components/Counter.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Counter() {
-  return (
-    <div>
-      <h1>Counter</h1>
-    </div>
-  )
+
+    const [counter, setCounter] = useState(0)
+    const [amount, setAmount] = useState(0)
+
+
+    const decreaseCounter = () => {
+        return (
+            setCounter(counter - amount)
+        )
+    }
+    const increaseCounter = () => {
+        return (
+            setCounter(counter + amount)
+        )
+    }
+    return (
+        <div>
+            {/* <h1>Amount: {amount[1]}</h1> */}
+            <h1>Welcome to Functionality Counter</h1>
+            <br />
+            <h1>Counter: {counter}</h1>
+            <button onClick={decreaseCounter}>Decrease(-{amount})</button>
+            <button onClick={increaseCounter}>Increase(+{amount})</button>
+            <br />
+            <button onClick={() => `${setCounter(0)} && ${setAmount(0)}`}>Reset</button>
+            <br />
+            <button onClick={() => setAmount(0)}>-+0</button>
+            <button onClick={() => setAmount(1)}>-+1</button>
+            <button onClick={() => setAmount(2)}>-+2</button>
+            <button onClick={() => setAmount(3)}>-+3</button>
+            <button onClick={() => setAmount(4)}>-+4</button>
+            <button onClick={() => setAmount(5)}>-+5</button>
+            <button onClick={() => setAmount(6)}>-+6</button>
+            <button onClick={() => setAmount(7)}>-+7</button>
+            <button onClick={() => setAmount(8)}>-+8</button>
+            <button onClick={() => setAmount(9)}>-+9</button>
+            <button onClick={() => setAmount(10)}>-+10</button>
+            <br />
+            <h2>Amount: -+{amount}</h2>
+        </div>
+    )
 }
 
 export default Counter
